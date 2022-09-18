f9632b87a714e6a268f998d997d089e532d69c1c
EssyK <kowuoche@gmail.com>
2022-09-18 15:41:13 +0300

    Add build folder for execution

---
+++ MarsRoverBuild/ExecuteCommand.ps1
@@ -0,0 +1,9 @@
+﻿$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$exePath = "$scriptPath\MarsRover.exe"
+$arguments = "5 5",
+                "1 2 N",
+                "LMLMLMLMM",
+                "3 3 E",
+                "MMRMMRMRRM"
+
+& $exePath $arguments
